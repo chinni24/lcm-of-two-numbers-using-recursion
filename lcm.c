58deef9dbff82cb0ad5708888225ef37387cd7d8
annavaram <annavaramdasari99@gmail.com>
2020-09-28 10:19:29 -0700

    created lcm.c
    this file prints lcm of any two numbers

---
+++ lcm.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int lcm(int a, int b);
+void main()
+{
+ int a, b, LCM;
+ printf("enter any two numbers to find LCM:);
+ scanf("%d%d", &a, &b);
+ if(a > b)
+   LCM = lcm(b, a);
+ else
+   LCM = lcm(a, b);
+ printf("LCM of %d and %d = %d", a,b);
+ getch();
+}
+int lcm(int a, int b)
+{
+ static int multiple = 0;
+ multiple += b;
+ if((multiple % a == 0) && (multiple % b == 0))
+ {
+  return multiple;
+ }
+ else
+ {
+  return lcm(a,b);
+ }
+}
+ 
